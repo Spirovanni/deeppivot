322c6ddd8eaa4091af67c579d6d165127e0c5939
Spirovanni <blackshieldsx@gmail.com>
2025-07-04 10:30:07 -0700

    Refactor HeroSection and Nav components to use motion/react for animations, and update Navbar implementation to be commented out for future use.

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { motion } from "motion/react";
 import { useLayoutEffect, useState } from "react";
 import HumeLogo from "./logos/Hume";
 import { Button } from "./ui/button";
@@ -27,28 +28,15 @@ export const Nav = () => {
   };
 
   return (
-    <div
-      className={
-        "fixed top-0 right-0 px-4 py-2 flex items-center h-14 z-50"
-      }
-    >
-      <div className={"ml-auto flex items-center gap-1"}>
-        <Button
-          onClick={() => {
-            window.open(
-              pkg.homepage,
-              "_blank",
-              "noopener noreferrer"
-            );
-          }}
-          variant={"ghost"}
-          className={"ml-auto flex items-center gap-1.5 rounded-full"}
-        >
-          <span>
-            <Github className={"size-4"} />
-          </span>
-          <span>Active Version 2</span>
-        </Button>
+    <nav className="flex w-full items-center justify-between border-t border-b border-neutral-200 px-4 py-4 dark:border-neutral-800">
+      {/* <div className={"ml-auto flex items-center gap-1"}> */}
+
+      <div className="flex items-center gap-2">
+        <div className="size-7 rounded-full bg-gradient-to-br from-violet-500 to-pink-500" />
+        <h1 className="text-base font-bold md:text-2xl">Deep Pivot</h1>
+      </div>
+
+
         <Button
           onClick={toggleDark}
           variant={"ghost"}
@@ -63,7 +51,10 @@ export const Nav = () => {
           </span>
           <span>{isDarkMode ? "Light" : "Dark"} Mode</span>
         </Button>
-      </div>
-    </div>
+        <button className="w-24 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 md:w-32 dark:bg-white dark:text-black dark:hover:bg-gray-200">
+        Login
+      </button>
+      {/* </div> */}
+    </nav>
   );
 };
